66b0e6b11aaf01099148deee926fa911e53c53f0
Wayne <wguo6358@gmail.com>
2022-10-08 23:54:21 +0000

    reformat

--- Front_End/src/pages/Home.tsx
+++ Front_End/src/pages/Home.tsx
@@ -9,44 +9,40 @@ setupIonicReact({
 
 var beforeChange: number;
 export default function Home() {
-  const [mywidth, setData] = useState(window.innerWidth);//not error just 0 makes it disapear
+  // const [text, setData] = useState(window.innerWidth);//not error just 0 makes it disapear
   // function handleResize(){
   //   if(window.innerWidth/window.innerHeight > 10/16)
   //   {
-  //     var tmp:number = mywidth;
+  //     var tmp:number = text;
   //     tmp = window.innerHeight*9/16;
   //     console.log("tmp",tmp)
-  //     setData(tmp)  //https://github.com/ReactJS-Concepts/ReactJS-Concepts/blob/main/reactjs-concepts/src/Code/Concepts/DataBinding/OneWayDataBinding.jsx
-  //     //find the error too many rendering self call kinda
+  //     setData(tmp)  
   //   }
-  //   else{
-  //     // mywidth = window.innerWidth;
-  //     // setData(mywidth)
-  //   }
-
   // }
 
   // handleResize();
-  // window.addEventListener('resize', handleResize); //not posible for loop 
+  window.addEventListener('resize', handleResize); //not posible for loop 
   return (
 
-      <IonPage style={{ width: Math.min(window.innerHeight * 9 / 16, window.innerWidth) ,        position: 'absolute', left: '50%', 
-      transform: 'translate(-50%, 0)'}}>
-        <IonHeader>
-          <IonToolbar>
-            <IonTitle>HappieEE</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <IonContent>
-          {/* <IonHeader collapse="condense">
+    <IonPage style={{
+      width: Math.min(window.innerHeight * 9 / 16, window.innerWidth), position: 'absolute', left: '50%',
+      transform: 'translate(-50%, 0)'
+    }}>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>HappieEE</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+        {/* <IonHeader collapse="condense">
           <IonToolbar>
             <IonTitle size="large">HappieEE</IonTitle>
           </IonToolbar> */}
-          {/* </IonHeader> */}
-          {/* {mywidth} */}
-          <ExploreContainer />
-        </IonContent>
-      </IonPage>
+        {/* </IonHeader> */}
+        {/* {mywidth} */}
+        <ExploreContainer />
+      </IonContent>
+    </IonPage>
 
   );
 
