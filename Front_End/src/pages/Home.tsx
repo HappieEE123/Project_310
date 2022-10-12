ae4e0e6e9bfaf9686bbf03d546ac2ba929411d2e
Wayne <wguo6358@gmail.com>
2022-10-12 02:02:30 +0000

    Added Camera

--- Front_End/src/pages/Home.tsx
+++ Front_End/src/pages/Home.tsx
@@ -1,4 +1,5 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, setupIonicReact } from '@ionic/react';
+import { IonContent, IonFab, IonFabButton, IonIcon, IonHeader, IonPage, IonTitle, IonToolbar, setupIonicReact } from '@ionic/react';
+import { camera} from 'ionicons/icons';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 import React, { useState } from 'react';
@@ -37,12 +38,17 @@ export default function Home() {
         <IonHeader collapse="condense">
           <IonToolbar>
             <IonTitle size="large">HappieEE</IonTitle>
-          </IonToolbar> 
+          </IonToolbar>
         </IonHeader>
         {/* {mywidth} */}
         <ExploreContainer />
+        <IonFab vertical="bottom" horizontal="end" slot="fixed">
+          <IonFabButton routerDirection="forward">
+            <IonIcon icon={camera}></IonIcon>
+          </IonFabButton>
+        </IonFab>
       </IonContent>
-    </IonPage>
+    </IonPage >
 
   );
 
