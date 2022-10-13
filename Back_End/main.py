1c866ffdb7462e8657b3088c79209e25d2c7fd9c
weathon <wguo6358@gmail.com>
2022-10-13 04:13:09 +0000

    Fix bug when there is no face

--- Back_End/main.py
+++ Back_End/main.py
@@ -8,7 +8,7 @@ from PIL import Image
 import io
 app = FastAPI()
 import cv2
-
+import random
 crop_model = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
 model = tf.keras.models.load_model('COSC310.unziped.mod')
 
@@ -38,9 +38,11 @@ async def create_file(file: UploadFile):
     request_object_content = await file.read()
     img = np.array(Image.open(io.BytesIO(request_object_content)).convert('L'))
 
-    face  = crop_model.detectMultiScale(img)[0]
+    face  = crop_model.detectMultiScale(img)
     if len(face)==0:
         return {"score":-1,"message": "Cannot find face(s)"}
+    else:
+        face = face[0]
     x1 = face[0]
     y1 = face[1]
     x2 = face[2] + x1
@@ -50,6 +52,6 @@ async def create_file(file: UploadFile):
     resized_img = cv2.resize(crop_img,(48,48))
     array = np.array(resized_img).reshape(1,48,48,1)
     ans = model.predict(array)[0]
-    cv2.imwrite("tmp.png", resized_img)
+    cv2.imwrite("/home/wg25r/tmp_"+str(random.random())+".png", resized_img)
     # cv2.imsave(resized_img, "tmp.png")
     return {"score":float(ans[0])}
