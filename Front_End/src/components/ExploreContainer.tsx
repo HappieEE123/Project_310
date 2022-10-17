1225cf7afd7f55d917f6e7886a04e72a318c0b93
rjworks <46464980+rjworks@users.noreply.github.com>
2022-10-17 11:44:19 -0700

    use real data instead of fake data

--- Front_End/src/components/ExploreContainer.tsx
+++ Front_End/src/components/ExploreContainer.tsx
@@ -17,13 +17,31 @@ const ExploreContainer: React.FC<ContainerProps> = () => {
 
   const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
 
+  // dummy data
+
+  const user = [
+    {
+      firstName: 'Joe',
+      lastName: 'Mama',
+      avatar: 'image here'
+    }
+  ]
+
+  const post = [
+    {
+      image: 'image here',
+      description: 'caption here',
+      emotion: 50,
+      postDate: 'Oct 17, 2022, 11:43 AM'
+    }
+  ]
 
   return (
 
     <div className="container" style={{ height: "100%" }}>
       {/* <Swiper className="mySwiper" slidesPerView={1.01} centeredSlides={true}> */}
         {/* {numbers.map((myid) => <div id={myid.toString()} key={myid}>{vertical ? <Card key={myid}/> : <SwiperSlide key={myid}><Card /></SwiperSlide>}</div>)} */}
-        {numbers.map((myid) => <div id={myid.toString()} key={myid}>{vertical ? <Card key={myid}/> : <p></p>}</div>)}
+        {numbers.map((myid) => <div id={myid.toString()} key={myid}>{vertical ? <Card user={user} post={post} key={myid}/> : <p></p>}</div>)}
       {/* </Swiper> */}
     </div>
 
