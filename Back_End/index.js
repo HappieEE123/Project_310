a732c41a082b9257e21d18534ea19bb904bacfaf
rjworks <46464980+rjworks@users.noreply.github.com>
2022-10-17 10:59:30 -0700

    check for crashes every 2 mins

--- Back_End/index.js
+++ Back_End/index.js
@@ -1,21 +1,23 @@
 import axios from 'axios';
-import {exec} from 'child_process';
-const path = "~/Project_310/Back_End/run.sh";
+import { exec } from 'child_process';
+const path = "/workspace/Project_310/Back_End/run.sh";
 
 await axios.get('api.weasoft.com/heartbeat')
   .then(function (response) {
-    return response.status
+    return response.status;
   })
   .catch(function (error) {
+    setInterval(function () {
       exec("nohup bash " + path + " &", (error, stdout, stderr) => {
         if (error) {
-            console.log(`error: ${error.message}`);
-            return;
+          console.log(`error: ${error.message}`);
+          return;
         }
         if (stderr) {
-            console.log(`stderr: ${stderr}`);
-            return;
+          console.log(`stderr: ${stderr}`);
+          return;
         }
         console.log(`stdout: ${stdout}`);
-    });
-  })
+      });
+    }, 120 * 1000);
+  });
