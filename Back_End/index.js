77d093d35190453d33f51cd1df5854cb4e209005
rjworks <46464980+rjworks@users.noreply.github.com>
2022-10-17 11:01:20 -0700

    fix interval

--- Back_End/index.js
+++ Back_End/index.js
@@ -2,12 +2,12 @@ import axios from 'axios';
 import { exec } from 'child_process';
 const path = "/workspace/Project_310/Back_End/run.sh";
 
-await axios.get('api.weasoft.com/heartbeat')
-  .then(function (response) {
-    return response.status;
-  })
-  .catch(function (error) {
-    setInterval(function () {
+setInterval(async function () {
+  await axios.get('api.weasoft.com/heartbeat')
+    .then(function (response) {
+      return response.status;
+    })
+    .catch(function (error) {
       exec("nohup bash " + path + " &", (error, stdout, stderr) => {
         if (error) {
           console.log(`error: ${error.message}`);
@@ -19,5 +19,5 @@ await axios.get('api.weasoft.com/heartbeat')
         }
         console.log(`stdout: ${stdout}`);
       });
-    }, 120 * 1000);
-  });
+    });
+}, 120 * 1000);
