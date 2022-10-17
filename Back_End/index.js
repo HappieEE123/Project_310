696a6a076e8c35fe39b271f36abe26738db3bbf4
rjworks <46464980+rjworks@users.noreply.github.com>
2022-10-17 10:51:37 -0700

    create heart beat detector

---
+++ Back_End/index.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+import {exec} from 'child_process';
+const path = "/workspace/Project_310/Back_End/run.sh";
+
+await axios.get('api.weasoft.com/heartbeat')
+  .then(function (response) {
+    return response.status
+  })
+  .catch(function (error) {
+      exec("nohup bash " + path + " &", (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+    });
+  })
