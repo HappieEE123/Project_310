38ce80e0882a45b75e4622db602c9ecf35ba623f
kmahmoud1 <khaliddm212@gmail.com>
2022-10-25 04:32:18 +0000

    Finished First Version

--- Front_End/src/pages/Login.tsx
+++ Front_End/src/pages/Login.tsx
@@ -1,16 +1,19 @@
 import { IonContent, IonRow, IonCol, IonLabel, IonModal, IonButtons, useIonActionSheet, IonProgressBar, IonButton, IonItem, IonInput, IonList, IonBackButton, IonTextarea, IonFab, IonFabButton, IonIcon, IonHeader, IonPage, IonTitle, IonToolbar, setupIonicReact } from '@ionic/react';
-import { camera, addOutline, paperPlaneOutline } from 'ionicons/icons';
+import { camera, addOutline, paperPlaneOutline, personCircle } from 'ionicons/icons';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 import React, { useState } from 'react';
 import type { OverlayEventDetail } from '@ionic/core';
+
+
 
 setupIonicReact({
   mode: 'ios',
 });
 
 
-export default function Home() {
+
+export default function Login() {
 
   return (
     <IonPage style={{
@@ -24,45 +27,57 @@ export default function Home() {
       </IonHeader>
       <IonContent>
           
-      //render icon
+      
       <IonRow>
         <IonCol>
           <IonIcon
-          style={{ fontSize: "70px", color: "#0040ff" }}
+          style={{ fontSize: "100px", color: "#0040ff" }}
           icon={personCircle}
         />
         </IonCol>
       </IonRow>
         
-        //email component
+        
       <IonRow>
         <IonCol>
           <IonItem>
            <IonLabel position="floating"> Email</IonLabel>
            <IonInput
              type="email"
-             value={0}
-            onIonChange={(e) => setEmail(e.detail.value!)}
+             
+            
          >
            </IonInput>
           </IonItem>
         </IonCol>
       </IonRow>
-        
-        //helper text and login button 
+
       <IonRow>
         <IonCol>
-          <p style={{ fontSize: "small" }}>
-            By clicking LOGIN you agree to our <a href="#">Policy</a
-         </p>
-          <IonButton expand="block" onClick={handleLogin}>
-            Login
-          </IonButton>
-          <p style={{ fontSize: "medium" }}>
-           Don't have an account? <a href="#">Sign up!</a>
-          </p>
+          <IonItem>
+           <IonLabel position="floating"> Password</IonLabel>
+           <IonInput
+             
+            
+         >
+           </IonInput>
+          </IonItem>
         </IonCol>
       </IonRow>
+
+      <IonRow>
+  <IonCol>
+    <p style={{ fontSize: "small" }}>
+      By clicking LOGIN you agree to our <a href="#">Policy</a>
+    </p>
+    <IonButton expand="block" onClick={alert('login')}>
+      Login
+    </IonButton>
+    <p style={{ fontSize: "medium" }}>
+      Don't have an account? <a href="#">Sign up!</a>
+    </p>
+  </IonCol>
+</IonRow>
 
       </IonContent >
     </IonPage >
